d34bae02ef866ce709b2b3f853f1ad7474c6b084
aslamhus <aslam.percy.husain@gmail.com>
2022-09-27 23:39:44 -0700

    decouple method names from original project

--- useDarkModePreference.js
+++ useDarkModePreference.js
@@ -3,22 +3,21 @@ import { useEffect, useState, useCallback } from 'react';
 export const useDarkModePreference = () => {
   const [prefersDark, setPrefersDark] = useState(false);
 
-  const updateLogoColorScheme = useCallback(() => {
+  const updateColorScheme = () => {
     setPrefersDark(window.matchMedia('(prefers-color-scheme: dark)').matches);
-  }, []);
+  };
 
   useEffect(() => {
     let darkModePreference;
     if (window.matchMedia) {
       darkModePreference = window.matchMedia('(prefers-color-scheme: dark)');
-      darkModePreference.addEventListener('change', updateLogoColorScheme);
-
-      updateLogoColorScheme();
+      darkModePreference.addEventListener('change', updateColorScheme);
+      updateColorScheme();
     }
 
     return () => {
       if (darkModePreference) {
-        darkModePreference.removeEventListener('change', updateLogoColorScheme);
+        darkModePreference.removeEventListener('change', updateColorScheme);
       }
     };
   }, []);
